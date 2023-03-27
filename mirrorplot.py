63a87f639a4942795bed48c3446136634cf28759
Dcd-hhs <dcdoedens@hhs.nl>
2023-03-27 21:51:26 +0200

    main bewerkt

--- mirrorplot.py
+++ mirrorplot.py
@@ -22,6 +22,15 @@ from numpy import cos, sin, tan, arctan2 as atan, pi
 #% constants
 tau = 2*pi
 todeg=180/pi
+#plotbounds
+xsize = 3
+xmin = -1.5
+xmax = xmin+xsize
+ysize = 3
+ymin = -0.2
+ymax = ymin+ysize
+x=[]
+y=[]
 
 #% function defines
 def phi_in(time):
@@ -37,24 +46,36 @@ def phi_out(pos):
 
 def phi_mirror(time, d=-10):
     return (phi_in(time) + phi_out(d))/2
-#% shape defines
-def ray(pos):
-    #calc incoming ray bounds
-        
 
+#% shape defines
+def phi_corner(pos):
+    #position is always at h=0
+    xoff=-xmin-pos
+    yoff= ysize+ymin
+    print(xoff, yoff)
+    return atan(xoff,yoff)
+
+def ray(pos,hour):
+    #calc incoming ray bounds
+    ochtend = 0
+    if hour < 12:
+        msg = "laat"
+        if phi_in(hour)<phi_corner(pos) and ochtend == 1:
+        #calculate height
+            msg = "vroeg"
+        print(msg, "in de ochtend", phi_in(hour), phi_corner(pos))
+    #if phi_in(hour)>phi_corner(pos) and
 #% run the sucker
 def main():
-    x=[]
-    y=[]
-    t=range(6,18+1)
+    t= [3]#range(6,18+1)
+    d = np.linspace(xmin, xmax, 5)
     for hour in t:
-        print(phi_mirror(hour,0)*todeg)
-        x.append((phi_mirror(hour,0)*todeg))
-        y.append(hour)
-    print(x)
-    print(y)
-    plt.plot(x,y)
-    plt.show()
+        for pos in d:
+            x.append(pos)
+            y.append(0)
     return 1
 if __name__ == '__main__':
     main()
+    plt.plot(x,y,'.')
+    plt.xlim(xmin,xmax)
+    plt.ylim(ymin,ymax)
